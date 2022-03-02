639f028496a3dd67dd1d484cef72ebcf643c0fb2
Paramveer Singh <46756848+paul1204@users.noreply.github.com>
2022-03-01 22:06:05 -0500

    Added more Routings

--- app/src/main/java/com/example/squaresgamept2/MainActivity.java
+++ app/src/main/java/com/example/squaresgamept2/MainActivity.java
@@ -1,56 +1,35 @@
-package com.example.squaresgamept2;
+package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pausegame);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
+        setContentView(R.layout.mainmenu);
+
+        Button mainStart = (Button) findViewById(R.id.mainStart);
+        // Button mainStart = null;
+        mainStart.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), gameSetup.class);
+            startActivity(intent);
+            //openGameSettings();
+        });
+
+        Button mainSetting = (Button) findViewById(R.id.mainSettings);
+        mainSetting.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), generalSettings.class);
+            startActivity(intent);
+            //openGameSettings();
+        });
     }
 }
