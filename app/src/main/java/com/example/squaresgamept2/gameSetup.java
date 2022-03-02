09ab2c9ffe12bd2a18ea62cb06889263dbb2620b
Paramveer Singh <46756848+paul1204@users.noreply.github.com>
2022-03-01 22:08:23 -0500

    Refactored Anonymous Function to Lambda

--- app/src/main/java/com/example/squaresgamept2/gameSetup.java
+++ app/src/main/java/com/example/squaresgamept2/gameSetup.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,11 @@ public class gameSetup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamesetup);
+
+        Button startGame = (Button) findViewById(R.id.startGame);
+        startGame.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), game.class);
+            startActivity(intent);
+        });
     }
 }
