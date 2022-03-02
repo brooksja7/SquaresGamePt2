d85bfcc5adabf7db63a1396f7c229d2b221f3058
Kevin Ly <kevinly277@gmail.com>
2022-03-02 11:40:50 -0500

    hopefully this fixes the bugs

--- app/src/main/java/com/example/squaresgamept2/MainActivity.java
+++ app/src/main/java/com/example/squaresgamept2/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pausegame);
+        setContentView(R.layout.mainmenu);
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
