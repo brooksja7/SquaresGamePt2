51ff53fb7992659e845ad826256fc5d2fc5bc7e5
Kevin Ly <lykt2@vcu.edu>
2022-03-04 22:53:25 -0500

    Updated the mainmenu.xml and created buttons that'll lead to the appropriate activities

--- app/src/main/java/com/example/squaresgamept2/MainActivity.java
+++ app/src/main/java/com/example/squaresgamept2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.squaresgamept2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -12,6 +13,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,38 +21,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainmenu);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
+
+
+        /*****************************************************
+         * Create a start button that when clicked will lead
+         * the User to the game setup activity.
+         ****************************************************/
+        Button startBtn = findViewById(R.id.mainStart);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gameSetupIntent = new Intent(getApplicationContext(), gameSetup.class);
+                startActivity(gameSetupIntent);
+            }
+        });
+
+
+        /*****************************************************
+         * Create a settings button that when clicked will lead
+         * the User to the settings activity.
+         ****************************************************/
+        Button settingsBtn = findViewById(R.id.mainSettings);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent settingsIntent = new Intent(getApplicationContext(), gameSetup.class);
+                startActivity(settingsIntent);
+            }
+        });
+
     }
 }
