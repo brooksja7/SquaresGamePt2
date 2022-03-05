7e068e862b51bf4ba504c20b15c7bbde7ba2f140
Kevin Ly <lykt2@vcu.edu>
2022-03-05 00:21:20 -0500

    Updated the General settings buttons and changed the exit button color to red and added backend code for gameSetup

--- app/src/main/java/com/example/squaresgamept2/gameSetup.java
+++ app/src/main/java/com/example/squaresgamept2/gameSetup.java
@@ -12,10 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class gameSetup extends Activity {
 
+    static int numberOfPlayers;
+    int xCounter = 0;
+    int yCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gamesetup);
+
+        TextView xView = findViewById(R.id.xNum);
+        TextView yView = findViewById(R.id.yNum);
 
         Button exitBtn = findViewById(R.id.backGamesetup);
         exitBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,6 +30,72 @@ public class gameSetup extends Activity {
             public void onClick(View view) {
                 Intent mainmenuIntent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(mainmenuIntent);
+            }
+        });
+
+        Button player2 = findViewById(R.id.player2);
+        player2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfPlayers = 2;
+            }
+        });
+
+        Button player3 = findViewById(R.id.player3);
+        player3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfPlayers = 3;
+            }
+        });
+
+        Button player4 = findViewById(R.id.player4);
+        player4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberOfPlayers = 4;
+            }
+        });
+
+        Button xPlus = findViewById(R.id.xPlus);
+        xPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                xCounter += 1;
+                xView.setText(Integer.toString(xCounter));
+            }
+        });
+
+        Button xMinus = findViewById(R.id.xMinus);
+        xMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(xCounter > 0)
+                {
+                    xCounter -= 1;
+                }
+                xView.setText(Integer.toString(xCounter));
+            }
+        });
+
+        Button yPlus = findViewById(R.id.yPlus);
+        yPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                yCounter += 1;
+                yView.setText(Integer.toString(yCounter));
+            }
+        });
+
+        Button yMinus = findViewById(R.id.yMinus);
+        yMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(yCounter > 0)
+                {
+                    yCounter -= 1;
+                }
+                yView.setText(Integer.toString(yCounter));
             }
         });
 
--- app/src/main/java/com/example/squaresgamept2/generalSettings.java
+++ app/src/main/java/com/example/squaresgamept2/generalSettings.java
@@ -1,15 +1,82 @@
 package com.example.squaresgamept2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class generalSettings extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generalsettings);
+
+        Button exitBtn = findViewById(R.id.backGeneralSettings);
+        Button musicBinary = findViewById(R.id.musicbinary);
+        Button gameVolume = findViewById(R.id.gameSoundBinary);
+        Button saveBtn = findViewById(R.id.Save);
+        Switch darkModeSwitch = findViewById(R.id.darkModeSwitch);
+
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainmenuIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainmenuIntent);
+            }
+        });
+
+        musicBinary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (musicBinary.getText().equals("OFF"))
+                {
+                    musicBinary.setText(R.string.ON);
+                    musicBinary.setBackgroundResource(R.drawable.roundedbluebuttons);
+                }
+                else
+                {
+                    musicBinary.setText(R.string.OFF);
+                    musicBinary.setBackgroundResource(R.drawable.roundedredbuttons);
+                }
+            }
+        });
+
+        gameVolume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(gameVolume.getText().equals("OFF"))
+                {
+                    gameVolume.setText(R.string.ON);
+                    gameVolume.setBackgroundResource(R.drawable.roundedbluebuttons);
+                }
+                else
+                {
+                    gameVolume.setText(R.string.OFF);
+                    gameVolume.setBackgroundResource(R.drawable.roundedredbuttons);
+                }
+            }
+        });
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Settings are Saved!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
