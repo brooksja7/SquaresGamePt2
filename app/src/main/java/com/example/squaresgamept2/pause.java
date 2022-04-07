73208e6224f706a8a74070fae515c0d90a733eb4
Kevin Ly <lykt2@vcu.edu>
2022-04-07 15:13:42 -0400

    Added background to the game page and added button functionalities to the pause screen. I added some comments as well.

--- app/src/main/java/com/example/squaresgamept2/pause.java
+++ app/src/main/java/com/example/squaresgamept2/pause.java
@@ -3,6 +3,7 @@ package com.example.squaresgamept2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,5 +14,51 @@ public class pause extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pausegame);
+
+        /************************************************************************
+         *  Declare and Initialize the buttons found on the pause-game screen
+         *  Resume, settings, and exit button are on the pause screen.
+         */
+        Button resumeBtn = findViewById(R.id.RESUME);
+        Button settingsBtn = findViewById(R.id.pauseSetting);
+        Button exitBtn = findViewById(R.id.pauseExit);
+
+        /************************************************************************
+         *  When User clicks on the resume button, take the user back to the game page.
+         */
+
+        resumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnToGameIntent = new Intent(getApplicationContext(), game.class);
+                startActivity(returnToGameIntent);
+            }
+        });
+
+        /************************************************************************
+         *  When User clicks on the settings button, take the user to the general settings page.
+         */
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnToSettingsIntent = new Intent(getApplicationContext(), generalSettings.class);
+                startActivity(returnToSettingsIntent);
+            }
+        });
+
+        /************************************************************************
+         * When User clicks on the exit button, take the user to main menu page.
+         */
+
+        exitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent returnToMainMenuIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(returnToMainMenuIntent);
+            }
+        });
+
+
     }
 }
